0e3d236ea174e996afe7877c91c29a3c22a30bf2
ml-murphy <this.michaelmurphy@gmail.com>
2026-02-18 17:36:46 -0700

    added basic transition logic using HashMap, cannot test without implementing more of DFA.java

--- p1Files/fa/dfa/DFAState.java
+++ p1Files/fa/dfa/DFAState.java
@@ -1,7 +1,34 @@
 package fa.dfa;
 
+import java.util.HashMap;
+
 import fa.State;
 
 public class DFAState extends fa.State {
+    private HashMap<String, Character> transitions;
+    
+    public DFAState(String name) {
+        super(name);
+        this.transitions = new HashMap<>();
+    }
+    /**
+     * 
+     * @param toState state to transition to
+     * @param onSymb symbol to map this transition to
+     * @return true if no mapping previously existed
+     */
+    public boolean addTransition(String toState, char onSymb) {
+        /** In order to maintain determinism, we need to check to see
+         *  if there are any existing mappings from this state to any other
+         *  state given the same Σ. 
+         */
+        Character c = this.transitions.get(toState);
+
+        if(c == onSymb) {
+            return false;
+        }
+        this.transitions.put(toState, onSymb);
+        return true;
+    }
     
 }
